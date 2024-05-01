971697a93ad85952c17537e0eda695b3563ac8b2
GogoPoz <georgia.pozatzidou@gmail.com>
2024-05-01 18:49:44 +0300

    add airplane script

---
+++ Airplane.py
@@ -0,0 +1,33 @@
+class Airplane:
+    def __init__(self, id_num, company, airport):
+        self.id_num = id_num
+        self.company = company
+        self.air_type = air_type
+        self.flying = False
+        self.airport = airport
+        self.destination = None
+
+    #apogeiwsi me proorismo to airport
+    def take_off(self, airport):
+        self.flying = True
+        self.destination = airport
+
+    #prosgeiwsi(an vrisketai se ptisi)
+    def landing(self):
+        if not self.flying:
+            return false
+        else:
+            self.flying = False
+            self.airport = self.destination
+            return True
+
+    #airplane info
+    def info(self):
+        return f"Το αεροπλάνο {self.id_num}, τύπου: {self.air_type}, εταιρία {self.company}"
+
+    #print tin katastasi tou aeroplanou(an einai se ptisi i exei prosgeiwthei)
+    def __str__(self):
+        if self.flying:
+            return f"Κατάσταση: Σε πτήση προς το αεροδρόμιο {self.destination}"
+        else:
+            return f"Κατάσταση: Έχει προσγειωθεί στο αεροδρόμιο {self.airport}"
